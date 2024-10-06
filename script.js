68d7bc4ad1ca5ee45863f83c1a764a6943551c20
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-10-06 22:10:58 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//your code here!
+const list = document.getElementById('infi-list');
 
+function listItems(val) {
+    for (let i = 0; i < val; i++) {
+        const items = document.createElement("li");
+        items.innerText = `Item ${list.children.length + 1}`;
+        list.appendChild(items);
+    }
+}
+
+listItems(10);
+
+function handleScroll() {
+    if (list.clientHeight + list.scrollTop >= list.scrollHeight) {
+        listItems(2);
+    }
+}
+
+list.addEventListener("scroll", handleScroll);
